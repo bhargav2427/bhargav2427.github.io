ebbb811b098a565d2625496dfb03dd50408fd95c
bhargav2427 <akhani.bharga@gmail.com>
2021-01-03 15:14:06 +0530

    Responsive Website Started

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// function myFunction() {
+//   var button = document.getElementById("more");
+//   var moreproject = document.getElementById("more-projects");
+//   console.log("Hello");
+//   console.log(button.innerHTML);
+//   if (button.innerHTML === "More") {
+//     moreproject.style.display = "block";
+//     button.innerHTML = "Less";
+//   } else {
+//     moreproject.style.display = "none";
+//     button.innerHTML = "More";
+//   }
+// }
